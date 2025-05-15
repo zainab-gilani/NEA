556a042f15c902d1bcec504707ae9a8c2b3335b6
zainab-gilani <zainab.gilani@yahoo.com>
2025-05-15 11:17:08 +0400

    Moved files

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for UniMatch project.
+URL configuration for mysite project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
