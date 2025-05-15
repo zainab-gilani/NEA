091c3d86c12e52000545f429dae8a981daae3624
zainab-gilani <zainab.gilani@yahoo.com>
2025-05-15 11:07:18 +0400

    Added to views.py

--- users/views.py
+++ users/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from .forms import LoginForm
 
 # Create your views here.
 
 def login_view(request):
+    form = LoginForm
+
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            print("Username", username)
+            print("Password", password)
+
     return render(request, "users/login.html")
+#enddef
