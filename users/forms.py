d431d0380716c3be9ab6666fcbedd3a875c1df2e
zainab-gilani <zainab.gilani@yahoo.com>
2025-05-15 11:17:22 +0400

    Amended forms.py

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,20 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
-class LoginForm(forms.Form):
-    username = forms.CharField(label="Username", max_length=150)
-    password = forms.CharField(label="Password", widget=forms.PasswordInput)
-#endclass
+
+# Create your forms here.
+
+class NewUserForm(UserCreationForm):
+	email = forms.EmailField(required=True)
+
+	class Meta:
+		model = User
+		fields = ("username", "email", "password1", "password2")
+
+	def save(self, commit=True):
+		user = super(NewUserForm, self).save(commit=False)
+		user.email = self.cleaned_data['email']
+		if commit:
+			user.save()
+		return user
